53a9a31082228e0faca96c551c32793f5fe5f932
wusi <wusidn@163.com>
2016-03-02 22:27:03 +0800

    CMake架构

---
+++ Core/Src/mian.cpp
@@ -0,0 +1,50 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+
+const char * windiwTitle = "Hello World";
+
+void init(void);
+void display(void);
+void idle(void);
+
+int main(int argc, char ** argv)
+{
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_RGBA);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(200, 100);
+    glutCreateWindow(windiwTitle);
+
+    if(glewInit()){
+        exit(EXIT_FAILURE);
+    }
+
+    init();
+
+    glutDisplayFunc(display);
+
+    glutIdleFunc(idle);
+
+    glutMainLoop();
+
+}
+
+void init(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glFlush();
+}
+
+void idle(void)
+{
+    display();
+}
