29fb2f14cc14d02d33818524726631b469f46e23
wusidn <wusidn@163.com>
2016-07-22 17:57:12 +0800

    写两句注释

--- Engine/Src/Tools/Tool.cpp
+++ Engine/Src/Tools/Tool.cpp
@@ -6,25 +6,30 @@ using namespace std;
 
 namespace engine::tools{
     
+    //返回字符串实际长度（单位是一个英文字符）
     const unsigned getStringLength(const string & str, const string & coding){
         unsigned result = 0;
         if(coding == "utf-8"){
+            //根据utf-8编码规则判断一个字符占几个字节
             for(size_t i = 0; i < str.length();){
                 int length = 0;
                 unsigned short temp = str.at(i) & 0x00FF;
                 while((temp << length & 0x00FF) > 1 << 7){
                     length ++;
                 }
+                //大于０x80的字符是中文并且占两个英文字符的宽度
                 result += temp < 0x80 ? 1 : 2;
                 length = length < 1 ? 1 : length;
                 i += length;
             }
         }else{
+            //其他编码类型还没有做
             result = str.length();
         }
         return result;
     }
 
+    //宽字与普通字符转换　　初步测试无效...
     string wsToS(const wstring & ws)
     {
         string curLocale = setlocale(LC_ALL, NULL); // curLocale = "C";
